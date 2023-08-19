e5bb8ee16fc6452b7ca7a782ab2dbb0381cb5a61
nicholass003 <veganicholas123@gmail.com>
2023-08-19 09:19:34 +0700

    Add TextChanger

---
+++ src/nicholass003/nicholasleaderboard/entities/TopNPC.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ *       _      _           _                ___   ___ ____  
+ *      (_)    | |         | |              / _ \ / _ \___ \ 
+ * _ __  _  ___| |__   ___ | | __ _ ___ ___| | | | | | |__) |
+ *| '_ \| |/ __| '_ \ / _ \| |/ _` / __/ __| | | | | | |__ < 
+ *| | | | | (__| | | | (_) | | (_| \__ \__ \ |_| | |_| |__) |
+ *|_| |_|_|\___|_| |_|\___/|_|\__,_|___/___/\___/ \___/____/ 
+ *                                                           
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * @author nicholass003
+ * @link https://github.com/nicholass003/
+ * 
+ */
+
+
+declare(strict_types=1);
+
+namespace nicholass003\nicholasleaderboard\entities;
+
+use nicholass003\nicholasleaderboard\utils\TextChanger;
+use pocketmine\entity\Human;
+
+class TopNPC extends Human implements TextChanger
+{
+    public const ENTITY_TYPE_HUMAN = 0;
+    public const ENTITY_TYPE_TEXT = 1;
+    public string $title = "";
+    public string $text = "";
+
+    public function getEntityType(int $type) : int
+    {
+        return $type;
+    }
+
+    public function getTitle() : string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title) : void
+    {
+        $this->title = $title;
+    }
+
+    public function getText() : string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text) : void
+    {
+        $this->text = $text;
+    }
+}
